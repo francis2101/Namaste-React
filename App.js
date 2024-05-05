0df4ff8d7308d3535a8785d6419e88b4e40d4dbd
francis2101 <francis_knightriders@gmail.com>
2024-05-05 19:36:15 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,34 @@
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "Hello World From React !!!"
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "This is h1 tag"),
+//     React.createElement("h2", {}, "This is h2 Tag"),
+//   ])
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
+const parent = React.createElement("div",
+    {id:"parent"},React.createElement("div",
+        {id:"child"},[
+            React.createElement("h1",{},"I'm h1 Tag"),
+            React.createElement("h2",{},"I'm h2 Tag")
+        ]
+    )
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
