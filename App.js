0122f928f9b4e33e045d753ea09bbf4c5a1a155d
francis2101 <francis_knightriders@gmail.com>
2024-05-06 15:38:20 +0530

    episode-01

--- App.js
+++ App.js
@@ -20,15 +20,16 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(parent);
 
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 Tag"),
+    React.createElement("h2", {}, "I'm h2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This's h1 tag"),
+    React.createElement("h2", {}, "This's h2 tag"),
+  ]),
+]);
 
-const parent = React.createElement("div",
-    {id:"parent"},React.createElement("div",
-        {id:"child"},[
-            React.createElement("h1",{},"I'm h1 Tag"),
-            React.createElement("h2",{},"I'm h2 Tag")
-        ]
-    )
-)
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
